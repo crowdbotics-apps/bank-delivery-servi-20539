d3f1a1e9d4730bed14b284d0759d25f8bb224484
Crowdbotics <team@crowdbotics.com>
2020-09-21 23:14:28 +0000

    Initial commit.

---
+++ ios/bank_delivery_servi_20539Tests/bank_delivery_servi_20539Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface bank_delivery_servi_20539 : XCTestCase
+
+@end
+
+@implementation bank_delivery_servi_20539
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
